11261223428b6e3539ab3b0ea31f4445b4dc9d6a
RedBullDogE <gsb06092000@gmail.com>
2021-03-03 12:55:32 +0200

    Fix checkstyle warnings

--- src/main/java/shortener/databaseconnector/DatabaseConnector.java
+++ src/main/java/shortener/databaseconnector/DatabaseConnector.java
@@ -1,14 +1,13 @@
 package shortener.databaseconnector;
 
 import io.micronaut.context.BeanContext;
-
-import javax.inject.*;
+import javax.inject.Singleton;
 
 /**
  * A service that serves as an intermediary point between database and controllers.
- * <p>
- * Add mocked methods returning data needed for controllers here and retrieve the instance of this class as follows:
- * `DatabaseConnector dbc = DatabaseConnector.getInstance();`
+ *
+ * <p>Add mocked methods returning data needed for controllers here and retrieve the instance
+ * of this class as follows: `DatabaseConnector dbc = DatabaseConnector.getInstance();`
  */
 @Singleton
 public class DatabaseConnector {
