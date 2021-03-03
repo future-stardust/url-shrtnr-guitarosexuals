dbd99bba37060002e75e33ae71b9a43b593e6b0b
AshenNagamaki <ashennagamaki@protonmail.com>
2021-03-03 23:25:08 +0200

    #5 - Authentication - Create authentication draft version

--- src/main/java/shortener/users/AuthenticationProviderUserPassword.java
+++ src/main/java/shortener/users/AuthenticationProviderUserPassword.java
@@ -2,40 +2,34 @@ package shortener.users;
 
 import edu.umd.cs.findbugs.annotations.Nullable;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.security.authentication.AuthenticationException;
 import io.micronaut.security.authentication.AuthenticationFailed;
 import io.micronaut.security.authentication.AuthenticationProvider;
 import io.micronaut.security.authentication.AuthenticationRequest;
 import io.micronaut.security.authentication.AuthenticationResponse;
 import io.micronaut.security.authentication.UserDetails;
-import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
-import java.util.ArrayList;
-import javax.inject.Singleton;
+import java.util.Collections;
+import javax.inject.Inject;
 import org.reactivestreams.Publisher;
 
-/**
- * Micronaut authentication bean that contains authorization logic: ensures that a user is
- * registered in the system and password is right.
- */
-@Singleton
+
 public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+
+  @Inject
+  UsersStore store;
 
   @Override
   public Publisher<AuthenticationResponse> authenticate(
-      @Nullable HttpRequest<?> httpRequest,
-      AuthenticationRequest<?, ?> authenticationRequest
-  ) {
-
-    return Flowable.create(emitter -> {
-      if (authenticationRequest.getIdentity().equals("sherlock")
-          && authenticationRequest.getSecret().equals("password")) {
-        emitter
-          .onNext(new UserDetails((String) authenticationRequest.getIdentity(), new ArrayList<>()));
-        emitter.onComplete();
-      } else {
-        emitter.onError(new AuthenticationException(new AuthenticationFailed()));
-      }
-    }, BackpressureStrategy.ERROR);
+    @Nullable HttpRequest<?> httpRequest,
+    AuthenticationRequest<?, ?> req) {
+    String username = req.getIdentity().toString();
+    String password = req.getSecret().toString();
+    if (password.equals(store.getUserPassword(username))) {
+      UserDetails details = new UserDetails(username,
+        Collections.singletonList(store.getUserRole(username)));
+      return Flowable.just(details);
+    } else {
+      return Flowable.just(new AuthenticationFailed());
+    }
   }
 }
