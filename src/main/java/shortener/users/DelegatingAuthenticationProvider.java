af08e6a79146d0afdf8673ffa289272ff3b4d56c
RedBullDogE <gsb06092000@gmail.com>
2021-03-05 22:48:00 +0200

    #5 - UserRepository methods implementation and improving authentication module with basic validation

--- src/main/java/shortener/users/DelegatingAuthenticationProvider.java
+++ src/main/java/shortener/users/DelegatingAuthenticationProvider.java
@@ -9,6 +9,7 @@ import io.micronaut.security.authentication.AuthenticationResponse;
 import io.micronaut.security.authentication.UserDetails;
 import io.reactivex.Flowable;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import javax.inject.Inject;
 import org.reactivestreams.Publisher;
 
@@ -20,17 +21,30 @@ public class DelegatingAuthenticationProvider implements AuthenticationProvider 
   @Inject
   UserRepository userRepository;
 
+  /**
+   * Authentication method with basic validation: password check and user existing check.
+   *
+   * @param httpRequest TODO
+   * @param authRequest TODO
+   * @return UserDetails with token / auth exception
+   */
   @Override
   public Publisher<AuthenticationResponse> authenticate(
       @Nullable HttpRequest<?> httpRequest,
-      AuthenticationRequest<?, ?> req) {
-    String email = req.getIdentity().toString();
-    String password = req.getSecret().toString();
-    if (password.equals(userRepository.getUserPassword(email))) {
-      UserDetails details = new UserDetails(email, new ArrayList<>());
-      return Flowable.just(details);
-    } else {
-      return Flowable.just(new AuthenticationFailed());
+      AuthenticationRequest<?, ?> authRequest) {
+    String enteredEmail = authRequest.getIdentity().toString();
+    String enteredPassword = authRequest.getSecret().toString();
+
+    try {
+      String userPassword = userRepository.getUserPassword(enteredEmail);
+      if (enteredPassword.equals(userPassword)) {
+        UserDetails details = new UserDetails(enteredEmail, new ArrayList<>());
+        return Flowable.just(details);
+      } else {
+        return Flowable.just(new AuthenticationFailed("Wrong password"));
+      }
+    } catch (NoSuchElementException ignored) {
+      return Flowable.just(new AuthenticationFailed("No such user"));
     }
   }
 }
--- src/main/java/shortener/users/UserRepository.java
+++ src/main/java/shortener/users/UserRepository.java
@@ -1,50 +1,87 @@
 package shortener.users;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 import javax.inject.Singleton;
 import shortener.database.Repository;
 import shortener.database.User;
 
+/**
+ * A database repository for Users module.
+ *
+ * <p>TODO: Replace mocked data with actual db interaction.
+ */
 @Singleton
 class UserRepository implements Repository<User, Integer> {
 
-  private final ArrayList<User> users;
+  // TODO: solve the issue with hashmaps
+  private final HashMap<Integer, User> idUserHashMap;
+  private final HashMap<String, User> emailUserHashMap;
 
   public UserRepository() {
-    users = new ArrayList<User>(Arrays.asList(
-        new User(1, "drew", "qwerty123"),
-        new User(2, "max", "asdfgh")
-    ));
+    idUserHashMap = new HashMap<>();
+    emailUserHashMap = new HashMap<>();
+    User testUser1 = new User(1, "drew@ex.com", "qwerty123");
+    User testUser2 = new User(2, "max@mail.ru", "lolpasswd");
+
+    idUserHashMap.put(testUser1.id(), testUser1);
+    idUserHashMap.put(testUser2.id(), testUser2);
+    emailUserHashMap.put(testUser1.email(), testUser1);
+    emailUserHashMap.put(testUser2.email(), testUser2);
   }
 
   @Override
   public User[] search() {
-    return (User[]) users.toArray();
+    return (User[]) idUserHashMap.values().toArray();
   }
 
   @Override
   public User get(Integer pk) throws NoSuchElementException {
-    return null;
+    User user = idUserHashMap.get(pk);
+
+    if (user != null) {
+      return user;
+    }
+
+    throw new NoSuchElementException(
+        String.format("User with the specified pk (%d) not found", pk));
   }
 
   @Override
   public User create(User record) throws IllegalArgumentException {
-    return null;
+    if (idUserHashMap.containsKey(record.id()) || emailUserHashMap.containsKey(record.email())) {
+      throw new IllegalArgumentException("The specified value already exists");
+    }
+    idUserHashMap.put(record.id(), record);
+    return emailUserHashMap.put(record.email(), record);
   }
 
   @Override
   public User delete(Integer pk) throws NoSuchElementException {
-    return null;
+    User userToDelete = idUserHashMap.remove(pk);
+    if (userToDelete == null) {
+      throw new NoSuchElementException();
+    }
+
+    return emailUserHashMap.remove(userToDelete.email());
+  }
+
+  public User delete(String email) throws NoSuchElementException {
+    User userToDelete = emailUserHashMap.remove(email);
+    if (userToDelete == null) {
+      throw new NoSuchElementException();
+    }
+
+    return idUserHashMap.remove(userToDelete.id());
   }
 
   public String getUserPassword(String email) throws NoSuchElementException {
-    for (User user : users) {
-      if (user.email().equals(email)) {
-        return user.password();
-      }
+    User user = emailUserHashMap.get(email);
+    if (user == null) {
+      throw new NoSuchElementException(
+          String.format("User with the specified email (%s) not found", email));
     }
-    return null;
+
+    return user.password();
   }
 }
