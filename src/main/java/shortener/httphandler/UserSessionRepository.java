2cae1a3548b15119b9b9def1121cd3f6a7517d66
RedBullDogE <gsb06092000@gmail.com>
2021-03-09 18:09:04 +0200

    #5/#9 - change UserSessionRepository storage from hashmap to arraylist + change field for get/delete from userId to token

--- src/main/java/shortener/httphandler/UserSessionRepository.java
+++ src/main/java/shortener/httphandler/UserSessionRepository.java
@@ -1,54 +1,100 @@
 package shortener.httphandler;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import javax.inject.Singleton;
 import shortener.database.Repository;
 import shortener.database.UserSession;
 
+/**
+ * A database repository for UserSession entity.
+ *
+ * <p>TODO: Replace mocked data with actual db interaction.
+ */
 @Singleton
-public class UserSessionRepository implements Repository<UserSession, Integer> {
-  private final HashMap<Integer, UserSession> userSessionHashMap;
+public class UserSessionRepository implements Repository<UserSession, String> {
+
+  private final ArrayList<UserSession> userSessions;
 
   public UserSessionRepository() {
-    userSessionHashMap = new HashMap<>();
+    userSessions = new ArrayList<>();
+  }
+
+  /**
+   * UserSessionRepository constructor provided for testing purposes. TODO: should be deleted
+   *
+   * @param initialUserSessionList initial user sessions
+   */
+  public UserSessionRepository(UserSession[] initialUserSessionList) {
+    userSessions = new ArrayList<>(Arrays.asList(initialUserSessionList));
   }
 
   @Override
   public UserSession[] search() {
-    return userSessionHashMap.values().toArray(new UserSession[0]);
+    return userSessions.toArray(new UserSession[0]);
   }
 
+  /**
+   * Get user session by token.
+   *
+   * @param token access token
+   * @return found user session
+   * @throws NoSuchElementException if session with specified token not found
+   */
   @Override
-  public UserSession get(Integer userId) throws NoSuchElementException {
-    UserSession userSession = userSessionHashMap.get(userId);
-
-    if (userSession != null) {
-      return userSession;
+  public UserSession get(String token) throws NoSuchElementException {
+    for (UserSession userSession : userSessions) {
+      if (userSession.token().equals(token)) {
+        return userSession;
+      }
     }
 
-    throw new NoSuchElementException(String.format("User session with the specified ID (%d) does not exist.", userId));
+    throw new NoSuchElementException(
+        String.format("User session with the specified ID (%s) does not exist.", token));
   }
 
+  /**
+   * Create new user session in database. If user already has a session, new session replaces the
+   * old one.
+   *
+   * @param newUserSession UserSession object to be added
+   * @return added UserSession
+   */
   @Override
-  public UserSession create(UserSession userSession) {
-    if (userSessionHashMap.containsKey(userSession.userId())) {
-      userSessionHashMap.replace(userSession.userId(), userSession);
+  public UserSession create(UserSession newUserSession) {
+    // Get userId list of users who have active sessions
+    ArrayList<Integer> userIdList = (ArrayList<Integer>) userSessions.stream()
+        .map(UserSession::userId).collect(Collectors.toList());
+
+    if (userIdList.contains(newUserSession.userId())) {
+      userSessions.set(userIdList.indexOf(newUserSession.userId()), newUserSession);
     } else {
-      userSessionHashMap.put(userSession.userId(), userSession);
+      userSessions.add(newUserSession);
     }
 
-    return userSession;
+    return newUserSession;
   }
 
+  /**
+   * Delete token from database if it exists.
+   *
+   * @param token token to be deleted
+   * @return deleted token
+   * @throws NoSuchElementException if session with specified token not found
+   */
   @Override
-  public UserSession delete(Integer userId) {
-    UserSession userSession = userSessionHashMap.remove(userId);
-
-    if (userSession == null) {
-      throw new NoSuchElementException(String.format("User session with the specified ID (%d) does not exist.", userId));
+  public UserSession delete(String token) throws NoSuchElementException {
+    for (UserSession userSession : userSessions) {
+      if (userSession.token().equals(token)) {
+        userSessions.remove(userSession);
+        return userSession;
+      }
     }
 
-    return userSessionHashMap.remove(userSession.userId());
+    throw new NoSuchElementException(
+        String.format("User session with the specified ID (%s) does not exist.", token));
+
   }
 }
