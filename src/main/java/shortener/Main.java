05ac3141361d6a68680345440f31d23af2c1d57d
Oleh Serikov <oserikov@jerasoft.net>
2021-03-11 14:04:46 +0200

    Task #16 - Implement Database - LocalStorage

--- src/main/java/shortener/Main.java
+++ src/main/java/shortener/Main.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import io.micronaut.runtime.Micronaut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import shortener.database.LocalStorage;
 
 /**
  * This is a main entry point to the URL shortener.
@@ -15,7 +16,14 @@ public class Main {
   private static final Gson gson = new Gson();
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+  /**
+   * Main.
+   *
+   * @param args CL arguments
+   */
   public static void main(String[] args) {
+    logger.info("Synchronizing the Database");
+    LocalStorage.synchronize();
     logger.info("Starting the Micronaut...");
     Micronaut.run(Main.class, args);
   }
--- src/main/java/shortener/database/LocalStorage.java
+++ src/main/java/shortener/database/LocalStorage.java
@@ -1,2 +1,49 @@
-package shortener.database;public class LocalStorage {
+package shortener.database;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import shortener.database.tables.aliases;
+
+/**
+ * Local Storage for Database.
+ */
+public class LocalStorage {
+
+  private static final Path rootPath = Paths.get("./../../../../..").toAbsolutePath();
+  private static final String localPath = "data";
+  private static final File storageDir = new File(rootPath.toString(), localPath);
+  private static final String[] tables = {"aliases", "users", "usersSessions", "usersAliases"};
+
+  private static boolean isInited() {
+    return Files.exists(storageDir.toPath());
+  }
+
+  /**
+   * Inits local data directory.
+   */
+  private static void init() {
+    // create dir itself
+    storageDir.mkdir();
+
+    // create dirs for tables
+    for (String table : tables) {
+      File tableDir = new File(storageDir, table);
+      tableDir.mkdir();
+    }
+
+    // init all tables' inner content
+    aliases.init(storageDir);
+    // TODO: call init on all tables
+  }
+
+  /**
+   * Main routine.
+   */
+  public static void synchronize() {
+    if (!isInited()) {
+      init();
+    }
+  }
 }
