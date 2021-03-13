4abdad81d64929b64c6c88a93a47f27bd86964ec
Dmytrii Puzyr <slymahel@gmail.com>
2021-03-13 11:43:50 +0200

    #15 - Fix redirection tests

--- src/test/java/shortener/httphandler/RedirectControllerTest.java
+++ src/test/java/shortener/httphandler/RedirectControllerTest.java
@@ -12,13 +12,21 @@ import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import java.io.IOException;
 import javax.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import shortener.database.Database;
+import shortener.database.entities.Alias;
+import shortener.exceptions.database.NotFound;
 
 @MicronautTest
 public class RedirectControllerTest {
+
+  static final String urlPattern = "/r/%s";
 
   @Inject
   EmbeddedServer embeddedServer;
@@ -27,10 +35,18 @@ public class RedirectControllerTest {
   @Client("/")
   HttpClient client;
 
-  static final String urlPattern = "/r/%s";
+  @Inject
+  Database db;
+
+  @MockBean(Database.class)
+  public Database mockDb() {
+    return Mockito.mock(Database.class);
+  }
 
   @Test
-  void redirectPositive() {
+  void redirectPositive() throws IOException {
+    Mockito.when(db.get(Mockito.any(), Mockito.any()))
+        .thenReturn(new Alias("alias1", "http://example1.org", 1L, 0));
 
     MutableHttpRequest<Object> request = HttpRequest.GET(String.format(urlPattern, "alias1"));
     HttpResponse<Object> response = client.toBlocking().exchange(request);
@@ -40,17 +56,19 @@ public class RedirectControllerTest {
   }
 
   @Test
-  void redirectNegative() {
+  void redirectNegative() throws IOException {
+    Mockito.when(db.get(Mockito.any(), Mockito.any()))
+        .thenThrow(new NotFound("aliases", "NotFound"));
 
     MutableHttpRequest<Object> request = HttpRequest.GET(String.format(urlPattern, "NotFound"));
 
     Throwable notFoundException = Assertions.assertThrows(
-      HttpClientResponseException.class,
-      () -> client.toBlocking().exchange(
-        request,
-        Argument.of(String.class),
-        Argument.of(String.class)
-      )
+        HttpClientResponseException.class,
+        () -> client.toBlocking().exchange(
+            request,
+            Argument.of(String.class),
+            Argument.of(String.class)
+        )
     );
 
     assertThat(notFoundException.getMessage()).contains("Alias not found.");
